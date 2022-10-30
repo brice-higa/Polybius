c5a7433fabf17a3c73150668be76ce2f6871f4aa
brice-higa <bricehiga@gmail.com>
2022-10-30 15:12:29 -0700

    Added difficulty selection to OptionsPanel, added public vairable difficultyMultiplier to MenuButtons script, added nonfunctioning volume slider.
    Signed-off-by: brice-higa <bricehiga@gmail.com>

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -1,13 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MenuButtons : MonoBehaviour
 {
     
     public GameObject MenuPanel;
     public GameObject OptionsPanel;
+
+    public AudioMixer audioMixer;
+    public Slider volumeSlider;
+
+    public double difficultyMultiplier = 1;
 
     // Show only the Main Menu Panel at the start
     void Start()
@@ -48,4 +56,28 @@ public class MenuButtons : MonoBehaviour
     }
 
 
+    public void SetVolume(float sliderValue)
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetDifficultyEasy()
+    {
+        difficultyMultiplier = 0.75;
+    }
+
+    public void SetDifficultyMedium()
+    {
+        difficultyMultiplier = 1;
+    }
+
+    public void SetDifficultyHard()
+    {
+        difficultyMultiplier = 1.25;
+    }
 }
