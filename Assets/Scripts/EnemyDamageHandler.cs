a75e103952bbdfd27286b263d38b47e457fcb48f
brice-higa <bricehiga@gmail.com>
2022-11-06 14:56:33 -0800

    Fixed Indentation
    Signed-off-by: brice-higa <bricehiga@gmail.com>

--- Assets/Scripts/EnemyDamageHandler.cs
+++ Assets/Scripts/EnemyDamageHandler.cs
@@ -5,71 +5,80 @@ using static DamageHandler;
 using System;
 
 public class EnemyDamageHandler : MonoBehaviour
-    {
-       public int health = 1;
-       public float invulnPeriod = 0;
-       float invulnTimer = 0; //seconds of invulnerability after crashing.
-       int correctLayer = 0;
-       SpriteRenderer spriteRend;
-       public GameObject healthDrop;
-       private System.Random rnd = new System.Random();
-    
+{
+    public int health = 1;
+    public float invulnPeriod = 0;
+    float invulnTimer = 0; //seconds of invulnerability after crashing.
+    int correctLayer = 0;
+    SpriteRenderer spriteRend;
+    public GameObject healthDrop;
+    private System.Random rnd = new System.Random();
 
-       void Start(){
+
+    void Start()
+    {
         correctLayer = gameObject.layer;
 
         spriteRend = GetComponent<SpriteRenderer>();
         //This only gets the renderer on the parent object.
         //It doesn't work for children.
-        if(spriteRend == null){
+        if (spriteRend == null)
+        {
             spriteRend = transform.GetComponentInChildren<SpriteRenderer>();
 
-            if(spriteRend == null){
-                Debug.LogError("Object '"+gameObject.name+"' has no sprite renderer");
-                }
-            }
-        }
-
-        void OnTriggerEnter2D(){
-            Debug.Log("Trigger!");
-
-            
-            health--; //reduce health by 1 point
-            //invulnTimer = 1.5f; // 1.5 seconds of invulnerability after collision
-            gameObject.layer = 8; //changing to invulnerable layer
-
-        }
-
-        void Update() {
-            
-            invulnTimer -= Time.deltaTime; //reducing invulnerable time.
-            
-            if (invulnTimer <= 0)
+            if (spriteRend == null)
             {
-                gameObject.layer = correctLayer;
-                if(spriteRend != null){
-                    spriteRend.enabled = true;
-                }
+                Debug.LogError("Object '" + gameObject.name + "' has no sprite renderer");
             }
-            else 
+        }
+    }
+
+    void OnTriggerEnter2D()
+    {
+        Debug.Log("Trigger!");
+
+
+        health--; //reduce health by 1 point
+                  //invulnTimer = 1.5f; // 1.5 seconds of invulnerability after collision
+        gameObject.layer = 8; //changing to invulnerable layer
+
+    }
+
+    void Update()
+    {
+
+        invulnTimer -= Time.deltaTime; //reducing invulnerable time.
+
+        if (invulnTimer <= 0)
+        {
+            gameObject.layer = correctLayer;
+            if (spriteRend != null)
             {
-                if(spriteRend != null){
-                    spriteRend.enabled = !spriteRend.enabled;
-                }
+                spriteRend.enabled = true;
             }
-
-            if (health <= 0){
-                Die();
-                enemiesKilled++;
+        }
+        else
+        {
+            if (spriteRend != null)
+            {
+                spriteRend.enabled = !spriteRend.enabled;
             }
         }
 
-        void Die(){
-            Destroy(gameObject);
-            if (rnd.NextDouble() < 0.5f)
-                Instantiate(healthDrop, transform.position, Quaternion.identity);
-
+        if (health <= 0)
+        {
+            Die();
+            enemiesKilled++;
         }
+    }
 
-        
+    void Die()
+    {
+        Destroy(gameObject);
+        if (rnd.NextDouble() < 0.5f)
+            Instantiate(healthDrop, transform.position, Quaternion.identity);
+
+    }
+
+
 }
