eef969a7a4598020949d3d70d81719ca4d3ffff0
brice-higa <bricehiga@gmail.com>
2022-11-06 14:55:07 -0800

    Fixed Indentation
    Signed-off-by: brice-higa <bricehiga@gmail.com>

--- Assets/Scripts/DamageHandler.cs
+++ Assets/Scripts/DamageHandler.cs
@@ -3,100 +3,112 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DamageHandler : MonoBehaviour
-    {
-       public int health = 1;
-       public float invulnPeriod = 0;
-       float invulnTimer = 0; //seconds of invulnerability after crashing.
-       int correctLayer = 0;
-       int maxHealth = 30;
-       public static int enemiesKilled = 0;
-       private int copper = 0;
-       private int iron = 0;
-       SpriteRenderer spriteRend;
-    
+{
+    public int health = 1;
+    public float invulnPeriod = 0;
+    float invulnTimer = 0; //seconds of invulnerability after crashing.
+    int correctLayer = 0;
+    int maxHealth = 30;
+    public static int enemiesKilled = 0;
+    private int copper = 0;
+    private int iron = 0;
+    SpriteRenderer spriteRend;
 
-       void Start(){
+
+    void Start()
+    {
         correctLayer = gameObject.layer;
 
         spriteRend = GetComponent<SpriteRenderer>();
         //This only gets the renderer on the parent object.
         //It doesn't work for children.
-        if(spriteRend == null){
+        if (spriteRend == null)
+        {
             spriteRend = transform.GetComponentInChildren<SpriteRenderer>();
 
-            if(spriteRend == null){
-                Debug.LogError("Object '"+gameObject.name+"' has no sprite renderer");
-                }
+            if (spriteRend == null)
+            {
+                Debug.LogError("Object '" + gameObject.name + "' has no sprite renderer");
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        Debug.Log("Trigger by " + other.gameObject.tag);
+
+        if (other.gameObject.tag == "healthTag")
+        {
+            health = health + 5;
+            if (health > maxHealth)
+                health = maxHealth;
+
+            Debug.Log("Trigger by " + other.gameObject + " and increased health by 5");
+        }
+        else if (other.gameObject.tag == "copperTag")
+        {
+            copper++;
+        }
+        else if (other.gameObject.tag == "ironTag")
+        {
+            iron++;
+        }
+        else
+        {
+            health--; //reduce health by 1 point
+            invulnTimer = 1.5f; // 1.5 seconds of invulnerability after collision
+            gameObject.layer = 8; //changing to invulnerable layer
+        }
+
+
+    }
+
+    void Update()
+    {
+
+        invulnTimer -= Time.deltaTime; //reducing invulnerable time.
+
+        if (invulnTimer <= 0)
+        {
+            gameObject.layer = correctLayer;
+            if (spriteRend != null)
+            {
+                spriteRend.enabled = true;
+            }
+        }
+        else
+        {
+            if (spriteRend != null)
+            {
+                spriteRend.enabled = !spriteRend.enabled;
             }
         }
 
-        void OnTriggerEnter2D(Collider2D other){
-            
-            Debug.Log("Trigger by " + other.gameObject.tag);
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
 
-            if(other.gameObject.tag == "healthTag")
-            {
-                health = health + 5;
-                if(health > maxHealth)
-                    health = maxHealth;
+    void Die()
+    {
+        Destroy(gameObject);
+    }
 
-                Debug.Log("Trigger by " + other.gameObject + " and increased health by 5");
-            }
-            else if(other.gameObject.tag == "copperTag")
-            {
-                copper++;
-            }
-            else if(other.gameObject.tag == "ironTag")
-            {
-                iron++;
-            }
-            else
-            {
-                health--; //reduce health by 1 point
-                invulnTimer = 1.5f; // 1.5 seconds of invulnerability after collision
-                gameObject.layer = 8; //changing to invulnerable layer
-            }
-            
-
+    void OnGUI()
+    {
+        if (health > 0)
+        {
+            GUI.Label(new Rect(0, 0, 100, 50), "Health: " + health);
+            GUI.Label(new Rect(0, 15, 100, 50), "Score: " + enemiesKilled);
+            GUI.Label(new Rect(0, 30, 100, 50), "Copper: " + copper);
+            GUI.Label(new Rect(0, 45, 100, 50), "Iron: " + iron);
+        }
+        else
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 25, 100, 50), "GAME OVER");
         }
 
-        void Update() {
-            
-            invulnTimer -= Time.deltaTime; //reducing invulnerable time.
-            
-            if (invulnTimer <= 0)
-            {
-                gameObject.layer = correctLayer;
-                if(spriteRend != null){
-                    spriteRend.enabled = true;
-                }
-            }
-            else 
-            {
-                if(spriteRend != null){
-                    spriteRend.enabled = !spriteRend.enabled;
-                }
-            }
-
-            if (health <= 0){
-                Die();
-            }
-        }
-
-        void Die(){
-            Destroy(gameObject);
-        }
-
-        void OnGUI(){
-            if(health > 0){
-            GUI.Label(new Rect(0,0,100,50), "Health: " + health);
-            GUI.Label(new Rect(0,15,100,50), "Score: " + enemiesKilled);
-            GUI.Label(new Rect(0,30,100,50), "Copper: " + copper);
-            GUI.Label(new Rect(0,45,100,50), "Iron: " + iron);
-            }
-            else{
-                GUI.Label(new Rect(Screen.width/2 - 50, Screen.height/2 - 25,100,50), "GAME OVER");
-            }
-
-        }
+    }
 }
