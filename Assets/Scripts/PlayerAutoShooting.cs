6bec3992783ad897c933c7eb06a65e5d34778edb
brice-higa <bricehiga@gmail.com>
2022-11-27 19:42:16 -0800

    Wrote some code for PlayerAutoShooting, it doesn't work.

--- Assets/Scripts/PlayerAutoShooting.cs
+++ Assets/Scripts/PlayerAutoShooting.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class PlayerAutoShooting : MonoBehaviour
 {
+
+    public Vector3 bulletOffset = new Vector3(0, 1f, 0);
+    public GameObject bulletPrefab;
+    int bulletLayer;
+    public float fireDelay = 0.5f;
+    float coolDownTimer = 0;
+    public GameObject myTarget;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+        // Decrease cooldown
+        coolDownTimer -= Time.deltaTime;
+
+        // Find all enemies
+        var targets = gameObject.FindGameOjbectsWithTag("Enemy");
+
+        // Find nearest enemy
+        var maxDistance = 100000;
+        for (var enemy : GameObject in targets)
+        {
+            var distance = Vector3.Distance(enemy.transform.position, transform.position);
+            if (distance < maxDistance)
+            {
+                mytarget = enemy;
+                maxDistance = distance;
+            }
+        }
+
+        // Shoot at enemy, is cooldown is up
+        if (coolDownTimer <= 0)
+        {
+            // Reset cooldown
+            coolDownTimer = fireDelay;
+
+            GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, myTarget.transform.position, transform.rotation);
+            bulletGO.layer = bulletLayer;
+
+        }
+        */
     }
 }
