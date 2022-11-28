ade5e36e3b819126297568dae6cc400f21221d61
brice-higa <bricehiga@gmail.com>
2022-11-27 18:56:22 -0800

    Fixed indentation on PlayerShooting, added blank PlayerAutoShooting script to implement automatic shooting and aiming
    Signed-off-by: brice-higa <bricehiga@gmail.com>

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -3,27 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
-{   
+{
 
-    public Vector3 bulletOffset =  new Vector3(0, 1f, 0);
+    public Vector3 bulletOffset = new Vector3(0, 1f, 0);
     public GameObject bulletPrefab;
     int bulletLayer;
     public float fireDelay = 0.5f;
     float coolDownTimer = 0;
 
-     void Start(){
+    void Start()
+    {
         bulletLayer = gameObject.layer;
     }
 
 
     // Update is called once per frame
-    void Update(){
+    void Update()
+    {
         coolDownTimer -= Time.deltaTime;
 
-        if(Input.GetButton("Fire1") && coolDownTimer <= 0){
+        if (Input.GetButton("Fire1") && coolDownTimer <= 0)
+        {
             //Fire:
-            
-            Debug.Log("FIREEEEE");
+
+            //Debug.Log("FIREEEEE");
             coolDownTimer = fireDelay;
 
             Vector3 offset = transform.rotation * bulletOffset;
